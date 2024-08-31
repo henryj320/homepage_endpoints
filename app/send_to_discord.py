981979620d351aab6dc7832d70fa755fdf6d94e0
Henry James <hj_320@protonmail.com>
2024-08-31 21:12:03 +0100

    Added click so that the Python can be called from terminal

--- app/send_to_discord.py
+++ app/send_to_discord.py
@@ -1,11 +1,14 @@
+"""Module to use Discord webhooks to send a Discord message."""
+
 import asyncio
-from discord import Webhook
+import click
+from discord import Webhook, errors
 import aiohttp
 # import requests
 # from pydantic import BaseModel
 
-async def send_discord_webhook(username: str, message: str, webhook_url: str):
-    """Generic function to send a Discord message to any Discord webhook
+async def send_discord_webhook(username: str, message: str, webhook_url: str) -> bool:
+    """Generic function to send a Discord message to any Discord webhook.
 
     Args:
         username (str): Username that the message should appear to come from.
@@ -13,19 +16,35 @@ async def send_discord_webhook(username: str, message: str, webhook_url: str):
         webhook_url (str): URL of the Discord webhook.
 
     Returns:
-        _type_: _description_
+        bool: True if send was successful.
     """
 
     # webhook_url = 'https://discord.com/api/webhooks/1232449180471918624/8p8DZ6AP0Mp6xLwKKM3KaCN-seJhxTd0k8ge0F1smO2DLVX5HniNvyFV2oz2bcM3UmlA'
-    async with aiohttp.ClientSession() as session:
-        webhook = Webhook.from_url(webhook_url, session=session)
-        await webhook.send(message, username=username)
+    try:
+        async with aiohttp.ClientSession() as session:
+            webhook = Webhook.from_url(webhook_url, session=session)
+            await webhook.send(message, username=username)
+        return True
+    except errors.HTTPException:
+        return False
 
-    return True
+
+
+@click.command()
+@click.argument('username', type=str, metavar='<username>', nargs=1)
+@click.argument('message', type=str, metavar='<message>', nargs=1)
+@click.argument('webhook_url', type=str, metavar='<webhook_url>', nargs=1)
+def main(username, message, webhook_url):
+    """Send a message to a Discord webhook.
+
+    Args:
+        username (str): Username that the message should appear to come from.
+        message (str): Contents of the message sent.
+        webhook_url (str): URL of the Discord webhook.
+    """
+    result = asyncio.run(send_discord_webhook(username, message, webhook_url))
+    print("Message sent:", result)
 
 if __name__ == '__main__':
-    webhook_url = 'https://discord.com/api/webhooks/1232449180471918624/8p8DZ6AP0Mp6xLwKKM3KaCN-seJhxTd0k8ge0F1smO2DLVX5HniNvyFV2oz2bcM3UmlA'
-
-    result = asyncio.run(send_discord_webhook("Test Username", "This is a message", webhook_url))
-    print(result)
-
+    URL = 'https://discord.com/api/webhooks/1232449180471918624/8p8DZ6AP0Mp6xLwKKM3KaCN-seJhxTd0k8ge0F1smO2DLVX5HniNvyFV2oz2bcM3UmlA'
+    main()  # pylint: disable=no-value-for-parameter
